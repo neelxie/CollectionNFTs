edbfc2db0f56254a2886373c36624f54794e22b0
neelxie <kidricederek@gmail.com>
2022-05-29 20:53:50 +0300

    Setup project

---
+++ hardhat-side/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x8d890d36592c75fdaa6c70ab8b76aebedc4cc098";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelisting-dapp-psi.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
