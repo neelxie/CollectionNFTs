b5c3c8107acbe452c11fb43ab7584a8216a2363c
neelxie <kidricederek@gmail.com>
2022-05-29 21:43:46 +0300

    Add collection to opensea

--- hardhat-side/constants/index.js
+++ hardhat-side/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x8d890d36592c75fdaa6c70ab8b76aebedc4cc098";
+const WHITELIST_CONTRACT_ADDRESS = "0xAB6A0d1CdB8d2d1733893C05C7CcC9FF3De746BE";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://whitelisting-dapp-psi.vercel.app/";
+const METADATA_URL = "https://collection-nft-s.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
